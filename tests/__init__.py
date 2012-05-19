db2418d418f5a6ada5192c124bf1bcc3bb7f2a3b
Whatcott, Michael <mdwhatcott@gmail.com>
2012-05-18 20:39:19 -0600

    Simplified the example specs.

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-def raise_error(error, message):
-    """
-    This method serves to appease the code analysis tools of IDE's that object
-    (as they should) to statements after the raise keyword.
-    """
-    raise error(message)
