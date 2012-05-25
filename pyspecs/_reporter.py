4676990e52acfcd73b08569ab4b97fe4aabf5b58
Whatcott, Michael <mdwhatcott@gmail.com>
2012-05-24 21:16:04 -0600

    Including captured output in final report for specs that have failures or errors.

--- pyspecs/_reporter.py
+++ pyspecs/_reporter.py
@@ -36,37 +36,51 @@ class Reporter(object):
         pass
 
 
+ERRORS = 'Errors'
+FAILED = 'Failed'
+PASSED = 'Passed'
+
+
+
 class DotReporter(Reporter):
     def __init__(self, console, capture=None):
         Reporter.__init__(self, capture)
         self.console = console
         self.specs = list() # list of 'specs', each a list of ReportableSteps
-        self.results = OrderedDict.fromkeys(['Passed', 'Failed', 'Errors'])
+        self.results = OrderedDict.fromkeys([PASSED, FAILED, ERRORS])
         for key in self.results:
             self.results[key] = 0
 
     def success(self, spec_name, step, step_name):
-        self._log_step(spec_name, step, step_name)
+        output = self._retrieve_captured_output()
+        self._log_step(spec_name, step, step_name, output=output)
         if step == THEN_STEP:
             self.console.write('.')
-            self.results['Passed'] += 1
+            self.results[PASSED] += 1
 
     def failure(self, spec_name, step_name, exc_stuff):
-        self._log_step(spec_name, THEN_STEP, step_name, exc_stuff)
+        output = self._retrieve_captured_output()
+        self._log_step(spec_name, THEN_STEP, step_name, exc_stuff, output)
         self.console.write('x')
-        self.results['Failed'] += 1
+        self.results[FAILED] += 1
 
     def error(self, spec_name, step, step_name, exc_stuff):
-        self._log_step(spec_name, step, step_name, exc_stuff)
+        output = self._retrieve_captured_output()
+        self._log_step(spec_name, step, step_name, exc_stuff, output)
         self.console.write('e')
-        self.results['Errors'] += 1
+        self.results[ERRORS] += 1
 
-    def _log_step(self, spec_name, step, step_name, exc_stuff=None):
+    def _retrieve_captured_output(self):
+        output = self.captured.getvalue()
+        self.captured.buf = str()
+        return output
+
+    def _log_step(self, spec_name, step, step_name, exc_stuff=None, output=None):
         if not len(self.specs) or spec_name != self.specs[-1][-1].spec_name:
             self.specs.append(list())
 
         self.specs[-1].append(
-            ReportableStep(spec_name, step, step_name, exc_stuff))
+            ReportableStep(spec_name, step, step_name, exc_stuff, output))
 
     def finish(self):
         self.report_problematic_specs()
@@ -91,19 +105,37 @@ class DotReporter(Reporter):
 
 
 def format_spec_for_console(spec):
+    formatted = StringIO('  SUBJECT {0}\n'.format(spec[0].spec_name))
+
+    for step in spec:
+        formatted.write(format_step_for_console(step))
+
+    formatted.write(format_captured_output_for_console(spec))
+
+    return formatted.getvalue()
+
+
+def format_captured_output_for_console(spec):
     formatted = StringIO()
 
-    formatted.write(str('  SUBJECT {0}\n'.format(spec[0].spec_name)))
-    for step in spec:
-        format_step_for_console(formatted, step)
-
+    if any(s.exc_info for s in spec) and any(s.output for s in spec):
+        formatted.write('-' * 31 + ' Captured Output ' + '-' * 31 + '\n')
+        for step in spec:
+            if step.output:
+                formatted.write(str('| ({0} {1})\n'.format(
+                    step.step.upper(), step.step_name)))
+                pretty_output = '\n| '.join(step.output.split('\n'))
+                formatted.write('| ' + pretty_output + '\n')
+        formatted.write('-' * 79)
     formatted.write('\n\n')
 
     return formatted.getvalue()
 
 
-def format_step_for_console(formatted, step):
-    step_name = step.step.upper()\
+def format_step_for_console(step):
+    formatted = StringIO()
+
+    step_sequence = step.step.upper()\
         if step.step != 'collect steps'\
         else '.'
     exc_message = '- ' + repr(step.exception_value)\
@@ -111,23 +143,26 @@ def format_step_for_console(formatted, step):
         else ''
 
     formatted.write(str('{0:>9} {1} {2}\n'.format(
-        step_name, step.step_name, exc_message)))
+        step_sequence, step.step_name, exc_message)))
 
     if step.exc_info is None:
-        return
+        return formatted.getvalue()
 
     formatted.write(
           '        . ' +
         '\n        . '.join(step.format_exception()) +
         '\n')
 
+    return formatted.getvalue()
+
 
 class ReportableStep(object):
-    def __init__(self, spec_name, step, step_name, exc_info):
+    def __init__(self, spec_name, step, step_name, exc_info=None, output=None):
         self.spec_name = spec_name
         self.step = step
         self.step_name = step_name
         self.exc_info = exc_info
+        self.output = output or str()
 
     @property
     def exception_type(self):
